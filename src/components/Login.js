14227d6fc1d5642801f38d6b9b517cc8745552ba
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-03 14:02:36 +0300

    Made the form responsive

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,19 +5,19 @@ const Login = () =>
     return ( 
         <>
             <div className="d-flex justify-content-end">
-                <form action="">
-                    <h1 className="my-4">Login Form</h1>
-                    <div className="form-floating my-4 mx-3">
-                        <input type="email" className="form-control" id="username" placeholder="Enter your username"/>
+                <form action="" className="row g-4 m-5">
+                    <h1 className="my-3">Login Form</h1>
+                    <div className="col-md-12">
                         <label htmlFor="username" className="form-label">Username</label>
+                        <input type="email" className="form-control mt-2" id="username" placeholder="Enter your username"/>
                     </div>
-                    <div className="form-floating my-4 mx-3">
-                        <input type="password" className="form-control" id="password" placeholder="Enter your password"/>
+                    <div className="col-md-12">
                         <label htmlFor="password" className="form-label">Password</label>
+                        <input type="password" className="form-control mt-2" id="password" placeholder="Enter your password"/>
                     </div>
-                    <div classNameName="buttons">
-                        <button type="submit" className="btn btn-success text-center col-11 mx-3 my-3">Login</button>
-                        <NavLink to="/" className="col-11 btn btn-danger mx-3 my-3">Back</NavLink>
+                    <div classNameName="container buttons">
+                        <button type="submit" className="btn btn-success text-center col-12 my-2">Login</button>
+                        <NavLink to="/" className="col-12 btn btn-danger my-2">Back</NavLink>
                     </div>
                 </form> 
             </div>
