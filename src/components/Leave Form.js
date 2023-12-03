a11d6f847a3c59d273cffa5cd0a03806f3cb608f
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-03 16:14:09 +0300

    Added the leave request form

--- src/components/Leave Form.js
+++ src/components/Leave Form.js
@@ -1,20 +1,30 @@
-const LeaveForm = () => {
+import { NavLink } from "react-router-dom";
+
+const LeaveForm = () => 
+{
     return ( 
         <>
-            <h1 className="text-center mt-4">Leave Request Form</h1>
+            <h1 className="text-center text-uppercase mt-4">Leave Request Form</h1>
             <form action="" className="row g-4 m-5">
                 <div className="col-md-6">
                     <label htmlFor="firstName" className="form-label">First Name</label>
-                    <input type="text" name="" id="firstName" className="form-control" placeholder="First Name" required/>
+                    <input type="text"id="firstName" className="form-control" placeholder="First Name"  readOnly/>
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="lastName" className="form-label">Last Name</label>
-                    <input type="text" name="" id="lastName" className="form-control" required/>
+                    <input type="text" id="lastName" className="form-control" placeholder="Last Name"  readOnly/>
+                </div>
+                <div className="col-md-6">
+                    <label htmlFor="department" className="form-label">Department</label>
+                    <input type="text" id="department" className="form-control" placeholder="Department" readOnly required/>
+                </div>
+                <div className="col-md-6">
+                    <label htmlFor="availableDays" className="form-label">Available Leave Days</label>
+                    <input type="text" id="availableDays" className="form-control" placeholder="Available Leave Days" readOnly/>
                 </div>
                 <div className="col-md-12">
                     <label htmlFor="leaveType">Leave Type</label>
-                    <select name="" id="leaveType" className="form-select" required>
-                        <option value="#">Select leave type</option>
+                    <select id="leaveType" className="form-select" required>
                         <option value="normal">Normal Leave</option>
                         <option value="paternity">Paternity Leave</option>
                         <option value="sick">Sick Leave</option>
@@ -23,15 +33,15 @@ const LeaveForm = () => {
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="startDate">Start Date</label>
-                    <input type="date" name="" id="startDate" className="form-control" required/>
+                    <input type="date" id="startDate" className="form-control" required/>
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="endDate">End Date</label>
-                    <input type="date" name="" id="endDate" className="form-control" required/>
+                    <input type="date" id="endDate" className="form-control" required/>
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="fileAttachment">Attach file</label>
-                    <input type="file" name="" id="fileAttachment" className="form-control" required/>
+                    <input type="file" id="fileAttachment" className="form-control" required/>
                 </div>
                 <div className="col-md-12">
                     <label htmlFor="reason">Reason for leave</label>
@@ -39,6 +49,9 @@ const LeaveForm = () => {
                 </div>
                 <div className="col-md-12 text-center">
                     <button type="submit" className="btn btn-success">Submit request</button>
+                </div>
+                <div className="col-md-12 text-center">
+                    <NavLink to="/" className="btn btn-danger">Cancel</NavLink>
                 </div>
             </form>
         </>
