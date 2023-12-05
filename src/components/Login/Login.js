64801328db8f601f9b51f112cb7be5dc4285fe97
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-05 19:56:19 +0300

    Reduced padding between login form h1 and the username label

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -9,14 +9,14 @@ const Login = () =>
             <div className={`${FormStyling.parentFlex} ${BackgroundStyling.background}`}>
                 <form action="" className={`row g-4 mt-5 ms-4 me-4 bg-light ${FormStyling.form}`}>
                     <img src={MANImage} alt="MAN Logo" className={FormStyling.image}/>
-                    <h1 className={`my-3 text-center text-uppercase ${FormStyling.h1}`}>Login Form</h1>
-                    <div className="col-md-12">
+                    <h1 className={`my-2 text-center text-uppercase ${FormStyling.h1}`}>Login Form</h1>
+                    <div className="col-md-12 mt-3">
                         <label htmlFor="username" className="form-label fs-5">Username</label>
-                        <input type="text" className="form-control mt-3" id="username" placeholder="Enter your username" required/>
+                        <input type="text" className="form-control mt-2" id="username" placeholder="Enter your username" required/>
                     </div>
-                    <div className="col-md-12">
+                    <div className="col-md-12 mt-3">
                         <label htmlFor="password" className="form-label fs-5">Password</label>
-                        <input type="password" className="form-control mt-3" id="password" placeholder="Enter your password" required/>
+                        <input type="password" className="form-control mt-2" id="password" placeholder="Enter your password" required/>
                     </div>
                     <div className="container buttons">
                         <button type="submit" className="btn btn-success text-center col-12 mt-4">Login</button>
