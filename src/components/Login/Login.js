69b1d9bd0822d18f816fc0431fbab98435d92ba6
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-05 19:48:11 +0300

    Resized the size of the logo

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -8,7 +8,7 @@ const Login = () =>
         <>
             <div className={`${FormStyling.parentFlex} ${BackgroundStyling.background}`}>
                 <form action="" className={`row g-4 mt-5 ms-4 me-4 bg-light ${FormStyling.form}`}>
-                    <img src={MANImage} alt="MAN Logo"/>
+                    <img src={MANImage} alt="MAN Logo" className={FormStyling.image}/>
                     <h1 className={`my-3 text-center text-uppercase ${FormStyling.h1}`}>Login Form</h1>
                     <div className="col-md-12">
                         <label htmlFor="username" className="form-label fs-5">Username</label>
