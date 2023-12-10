c6a165abe117875fe24d15d3ddbd4ad3abfbc30f
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-10 19:56:40 +0300

    Reduced the height of the login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,20 +10,20 @@ const Login = () =>
                 <form action="" className={`row g-4 mx-4 bg-light ${FormStyling.form}`}>
                     <img src={MANImage} alt="MAN Logo" className={FormStyling.image}/>
                     <h1 className={`my-2 text-center text-uppercase ${FormStyling.h1}`}>Login Form</h1>
-                    <div className="col-md-12 mt-3">
+                    <div className="col-md-12 mt-2">
                         <label htmlFor="username" className="form-label fs-5">Username</label>
                         <input type="text" className="form-control mt-2" id="username" placeholder="Enter your username" required/>
                     </div>
-                    <div className="col-md-12 mt-3">
+                    <div className="col-md-12 mt-2">
                         <label htmlFor="password" className="form-label fs-5">Password</label>
                         <input type="password" className="form-control mt-2" id="password" placeholder="Enter your password" required/>
                     </div>
                     <div className={FormStyling.childFlex}>
-                        <button type="submit" className="btn btn-success text-center mt-2">Login</button>
-                        <NavLink to="/" className="btn btn-danger mt-2 ms-5">Back</NavLink>
+                        <button type="submit" className="btn btn-success text-center mt-1">Login</button>
+                        <NavLink to="/" className="btn btn-danger mt-1 ms-5">Back</NavLink>
                     </div>
                     <div className="container">
-                        <NavLink to="/forgot" className="col-12 btn btn-danger mt-4 mb-4">Forgot password?</NavLink>
+                        <NavLink to="/forgot" className="col-12 btn btn-danger mt-3 mb-3">Forgot password?</NavLink>
                     </div>
                 </form> 
             </div>
