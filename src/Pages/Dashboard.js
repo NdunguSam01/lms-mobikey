484df5c55f19af8873d77128ef8f2b6026d4c3d6
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-16 12:50:57 +0300

    Created a sidebar component and added icons to it. Making it responsive to follow

---
+++ src/Pages/Dashboard.js
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar/Navbar'
+import Sidebar from '../components/Sidebar/Side';
+const Dashboard = () => 
+{
+    return ( 
+        <>
+            <h1>Dashboard Page</h1>
+            <Navbar/>
+            <Sidebar/>
+        </>
+     );
+}
+ 
+export default Dashboard;
--- src/components/Sidebar/Side.js
+++ src/components/Sidebar/Side.js
@@ -1,5 +1,6 @@
 import SideStyles from './side.module.css'
 import Logo from '../../assets/images/Logo.png'
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => 
 {
@@ -8,19 +9,19 @@ const Sidebar = () =>
             <img src={Logo} alt='Mobikey Logo' className={`bg-light ${SideStyles.image}`}/>
             <div className={SideStyles.sideOption}>
                 <i className={`fa fa-home ${SideStyles.sideIcons}`}></i>
-                <h3>Dashboard</h3>
+                <NavLink to="/dashboad" className={SideStyles.navLink}>Dashboard</NavLink>
             </div>
             <div className={SideStyles.sideOption}>
                 <i className={`fa fa-calendar ${SideStyles.sideIcons}`}></i>
-                <h3>Leave</h3>
+                <NavLink to="/leave" className={SideStyles.navLink}>Leave</NavLink>
             </div>
             <div className={SideStyles.sideOption}>
                 <i className={`fa fa-clock-o ${SideStyles.sideIcons}`}></i>
-                <h3>Pending Requests</h3>
+                <NavLink to="/pending" className={SideStyles.navLink}>Pending Request</NavLink>
             </div>
             <div className={SideStyles.sideOption}>
                 <i className={`fa fa-user ${SideStyles.sideIcons}`}></i>
-                <h3>Employees</h3>
+                <NavLink to="/users" className={SideStyles.navLink}>Employees</NavLink>
             </div>
         </aside>
      );
