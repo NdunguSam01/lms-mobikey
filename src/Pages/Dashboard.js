4e6c6a2a7d623b7b140706637aa7f2168d569fa2
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-17 10:46:11 +0300

    Redesigned the navbar to include a button for opening and closing the side navigation

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,12 +1,19 @@
 import Navbar from '../components/Navbar/Navbar'
 import Sidebar from '../components/Sidebar/Side';
+import { useState } from 'react';
 
 const Dashboard = () => 
 {
+    const [sidebarOpen, setSidebarOpen]=useState(false)
+
+    const openSidebar= () =>
+    {
+        setSidebarOpen(!sidebarOpen)
+    }
     return ( 
         <>
-            <Navbar/>
-            <Sidebar/>
+            <Navbar openSidebar={openSidebar} sidebarOpen={sidebarOpen}/>
+            <Sidebar sidebarOpen={sidebarOpen}/>
         </>
      );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,23 @@
 import { NavLink } from "react-router-dom";
 import styles from './navbar.module.css'
-const Navbar = () => 
+
+const Navbar = ({openSidebar, sidebarOpen}) => 
 {
     return (  
-        <nav className={`nav d-flex justify-content-end bg-dark ${styles.navbar}`}>
-            <NavLink to="/profile" className={styles.link}>Profile</NavLink>
-            <NavLink to="/login" className={styles.link}>Sign Out</NavLink>
-        </nav>
+        <>
+            <nav className={styles.navbar}>
+                {
+                sidebarOpen ? 
+                    <i className={`fa fa-close ${styles.icon}`} onClick={openSidebar}></i> 
+                    :
+                    <i className={`fa fa-bars ${styles.icon}`} onClick={openSidebar}></i>
+                }
+                <span className={styles.navLinks}>
+                    <NavLink to="/profile" className={styles.link}>Profile</NavLink>
+                    <NavLink to="/login" className={styles.link}>Sign Out</NavLink>
+                </span>
+            </nav>
+        </>
     );
 }
  
