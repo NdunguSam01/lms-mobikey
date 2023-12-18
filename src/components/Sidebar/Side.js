02e7c58ad8ab1ae7d3224278d4e0fa106bbef65e
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-18 12:36:35 +0300

    Added a new employee section to the sidebar

--- src/components/Sidebar/Side.js
+++ src/components/Sidebar/Side.js
@@ -43,51 +43,19 @@ const Sidebar = ({sidebarOpen}) =>
                             </div>
                         </NavLink>
                     </li>
+                    <li>
+                        <NavLink to="/new-employee" className={SideStyles.navLink}>
+                            <div className={SideStyles.sideOption}>
+                                <i className={`fa fa-plus ${SideStyles.sideIcons}`}></i>
+                                <h3 className={SideStyles.h3}>New Employee</h3>
+                            </div>
+                        </NavLink>
+                    </li>
                 </ul>
             </aside>
             )}
         </>
     )
-    
-    // return ( 
-    //     <aside className={SideStyles.sideSection}>
-    //         <img src={Logo} alt='Mobikey Logo' className={`bg-light ${SideStyles.image}`}/>
-    //         <ul className={SideStyles.sidebarList}>
-    //             <li>
-    //                 <NavLink to="/dashboard" className={SideStyles.navLink}>
-    //                     <div className={SideStyles.sideOption}>
-    //                         <i className={`fa fa-home ${SideStyles.sideIcons}`}></i>
-    //                         <h3 className={SideStyles.h3}>Dashboard</h3>
-    //                     </div>
-    //                 </NavLink>
-    //             </li>
-    //             <li>
-    //                 <NavLink to="/leave" className={SideStyles.navLink}>
-    //                     <div className={SideStyles.sideOption}>
-    //                         <i className={`fa fa-calendar ${SideStyles.sideIcons}`}></i>
-    //                         <h3 className={SideStyles.h3}>Leave</h3>
-    //                     </div>
-    //                 </NavLink>
-    //             </li>
-    //             <li>
-    //                 <NavLink to="/pending" className={SideStyles.navLink}>
-    //                     <div className={SideStyles.sideOption}>
-    //                         <i className={`fa fa-clock-o ${SideStyles.sideIcons}`}></i>
-    //                         <h3 className={SideStyles.h3}>Employee Requests</h3>
-    //                     </div>
-    //                 </NavLink>
-    //             </li>
-    //             <li>
-    //                 <NavLink to="/employees" className={SideStyles.navLink}>
-    //                     <div className={SideStyles.sideOption}>
-    //                         <i className={`fa fa-user ${SideStyles.sideIcons}`}></i>
-    //                         <h3 className={SideStyles.h3}>Employees</h3>
-    //                     </div>
-    //                 </NavLink>
-    //             </li>
-    //         </ul>
-    //     </aside>
-    //  );
 }
  
 export default Sidebar;
