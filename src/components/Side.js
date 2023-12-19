cf1ee6505a6d9aa50ed9737d9277575467407f4d
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-19 15:42:21 +0300

    Created the dropdown links

--- src/components/Side.js
+++ src/components/Side.js
@@ -1,10 +1,22 @@
 import SideStyles from '../assets/css/sidebar.module.css'
+import DropdownStyles from '../assets/css/sidedropdowns.module.css'
 import Logo from '../assets/images/Logo.png'
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 const Sidebar = ({sidebarOpen}) => 
 {
-    
+    const [leaveDropdown, setLeaveDropdown]=useState(false)
+    const [employeeDropdown, setEmployeeDropdown]=useState(false)
+
+    const toggleLeaveDropdown= () =>
+    {
+        setLeaveDropdown(!leaveDropdown)
+    } 
+    const toggleEmployeeeDropdown= () =>
+    {
+        setEmployeeDropdown(!employeeDropdown)
+    }   
     return(
         <>
             <aside className={`${SideStyles.sideSection} ${sidebarOpen ? `${SideStyles.open}` : ''}`}>
@@ -18,13 +30,39 @@ const Sidebar = ({sidebarOpen}) =>
                             </div>
                         </NavLink>
                     </li>
-                    <li>
-                        <NavLink to="/leave" className={SideStyles.navLink}>
+                    <li className={`dropdown ${DropdownStyles.dropdown}`}>
+                        <NavLink to="#" onClick={toggleLeaveDropdown} className={SideStyles.navLink}>
                             <div className={SideStyles.sideOption}>
                                 <i className={`fa fa-calendar ${SideStyles.sideIcons}`}></i>
                                 <h3 className={SideStyles.h3}>Leave</h3>
+                                {leaveDropdown 
+                                    ? 
+                                        (<i className={`fa fa-angle-down ${SideStyles.sideIcons} ${DropdownStyles.dropdownIcon}`}></i>)
+                                    : 
+                                        (<i className={`fa fa-angle-right ${SideStyles.sideIcons} ${DropdownStyles.dropdownIcon}`}></i>) 
+                                }
                             </div>
                         </NavLink>
+                        {leaveDropdown && (
+                            <ul className={`dropdown-content ${DropdownStyles.dropdownContent} ${SideStyles.sidebarList}`}>
+                                <li>
+                                    <NavLink to="/leave-form" className={SideStyles.navLink}>
+                                        <div className={SideStyles.sideOption}>
+                                            <i className={`fa fa-calendar-plus-o ${SideStyles.sideIcons}`}></i>
+                                            <h3 className={SideStyles.h3}>Apply Leave</h3>
+                                        </div>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/leave-history" className={SideStyles.navLink}>
+                                        <div className={SideStyles.sideOption}>
+                                            <i className={`fa fa-history ${SideStyles.sideIcons}`}></i>
+                                            <h3 className={SideStyles.h3}>Leave History</h3>
+                                        </div>
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        )}
                     </li>
                     <li>
                         <NavLink to="/pending" className={SideStyles.navLink}>
@@ -34,13 +72,39 @@ const Sidebar = ({sidebarOpen}) =>
                             </div>
                         </NavLink>
                     </li>
-                    <li>
-                        <NavLink to="/employees" className={SideStyles.navLink}>
+                    <li className="dropdown">
+                        <NavLink to="#" onClick={toggleEmployeeeDropdown} className={SideStyles.navLink}>
                             <div className={SideStyles.sideOption}>
                                 <i className={`fa fa-user ${SideStyles.sideIcons}`}></i>
                                 <h3 className={SideStyles.h3}>Employees</h3>
+                                {employeeDropdown 
+                                    ? 
+                                        (<i className={`fa fa-angle-down ${SideStyles.sideIcons} ${DropdownStyles.dropdownIcon}`}></i>) 
+                                    : 
+                                        (<i className={`fa fa-angle-right ${SideStyles.sideIcons} ${DropdownStyles.dropdownIcon}`}></i>) 
+                                }
                             </div>
                         </NavLink>
+                        {employeeDropdown && (
+                            <ul className={`dropdown-content ${DropdownStyles.dropdownContent} ${SideStyles.sidebarList}`}>
+                                <li>
+                                    <NavLink to="/new employee" className={SideStyles.navLink}>
+                                        <div className={SideStyles.sideOption}>
+                                            <i className={`fa fa-user-plus ${SideStyles.sideIcons}`}></i>
+                                            <h3 className={SideStyles.h3}>New Employee</h3>
+                                        </div>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/view employees" className={SideStyles.navLink}>
+                                        <div className={SideStyles.sideOption}>
+                                            <i className={`fa fa-users ${SideStyles.sideIcons}`}></i>
+                                            <h3 className={SideStyles.h3}>View Employees</h3>
+                                        </div>
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        )}
                     </li>
                 </ul>
             </aside>
