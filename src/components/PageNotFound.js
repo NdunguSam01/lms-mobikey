5828a50eb57628a2864b819d77f6c8b71b454567
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-21 13:15:03 +0300

    Changed the go back button in the 404 page to redirect a user back to the previous page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,14 +1,16 @@
 import ErrorImage from '../assets/images/Error.png'
 import Styling from '../assets/css/error.module.css'
-import { NavLink } from 'react-router-dom';
+import { useNavigate  } from 'react-router-dom';
 const PageNotFound = () => 
 {
+    const navigate=useNavigate()
+
     return ( 
         <div className={Styling.errorMessage}>
             <img src={ErrorImage} alt="" />
             <h1>404- Page Not Found</h1>
             <p>The page you are looking for might have been removed, had its name changed or is temporarily unavailable</p>
-            <NavLink to="/dashboard" className="btn btn-primary">Back to dashboard</NavLink>
+            <button className="btn btn-primary" onClick={() => navigate(-1)}>Go back</button>
         </div>
      );
 }
