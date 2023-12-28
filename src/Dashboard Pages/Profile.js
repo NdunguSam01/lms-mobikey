b5448d1c380ff5e14240802376420a66f1981122
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-28 15:48:31 +0300

    Changed the profile page appearance

--- src/Dashboard Pages/Profile.js
+++ src/Dashboard Pages/Profile.js
@@ -2,8 +2,7 @@ const Profile = () =>
 {
     return ( 
         <>
-            <h1 className="text-center text-uppercase mt-4">user profile</h1>
-            <section className="row g-2 mt-3">
+            <section className="row g-2">
                 <h2 className="text-uppercase">General information</h2>
                 <div className="col-md-6">
                     <label htmlFor="firstName" className="form-label fs-6">First Name</label>
@@ -38,7 +37,7 @@ const Profile = () =>
                     <input type="text" name="role" id="role" className="form-control fs-6" readOnly/>
                 </div>
             </section>
-            <form action="" className="row g-2 mt-3">
+            <form action="" className="row g-2 mt-2">
                 <h2 className="text-uppercase">Change Password</h2>
                 <div className="col-md-4">
                     <label htmlFor="currentPassword" className="form-label fs-6">Current password</label>
