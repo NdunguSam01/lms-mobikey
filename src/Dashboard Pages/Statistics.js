55fc7d10f5d7f7dbc2eb08454f52d6a12561da1e
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-28 16:05:23 +0300

    Changed the profile page appearance

--- src/Dashboard Pages/Statistics.js
+++ src/Dashboard Pages/Statistics.js
@@ -6,7 +6,7 @@ const Statistics = () =>
 {
     return ( 
         <>
-            <h1 className="text-uppercase text-center">Welcome back, Samuel Muigai</h1>
+            <h1 className="text-uppercase text-center mt-2">Welcome back, Samuel Muigai</h1>
             <StatisticsTiles/>
             <div className="parent-flex">
                 <LeaveBreakdown/>
