e6fce5c3b405bcf471eaecb0833c603f1523f716
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-31 10:26:13 +0300

    Redone the side nav

--- src/Dashboard Components/Side.js
+++ src/Dashboard Components/Side.js
@@ -1,123 +1,37 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import SideStyles from '../assets/css/sidebar.module.css'
+import {Sidebar, SidebarHeader, SidebarBody, SidebarLogo, MenuBar, MenuItem, SubMenuBar} from "react-sidebar-pro";
 import Logo from '../assets/images/Logo.png'
-import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
+import { FaHome, FaCalendarAlt, FaCalendarPlus, FaRegCalendarAlt, FaUserPlus, FaUser , FaUsers,FaTable     } from "react-icons/fa";
+import { MdOutlinePendingActions } from "react-icons/md";
+import '../assets/css/sidebar.css'
 
-const Sidebar = ({sidebarOpen}) => 
+const Side = ({sidebarOpen}) => 
 {
-    const [leaveDropdown, setLeaveDropdown]=useState(false)
-    const [employeeDropdown, setEmployeeDropdown]=useState(false)
-
-    const toggleLeaveDropdown= () =>
-    {
-        setLeaveDropdown(!leaveDropdown)
-        if(employeeDropdown === true)
-        {
-            setEmployeeDropdown(false)
-        }
-    } 
-    const toggleEmployeeeDropdown= () =>
-    {
-        setEmployeeDropdown(!employeeDropdown)
-        if(leaveDropdown === true)
-        {
-            setLeaveDropdown(false)
-        }
-    }   
-    return(
+    return ( 
         <>
-            <aside className={`${SideStyles.sideSection} ${sidebarOpen ? `${SideStyles.open}` : ''}`}>
-                <img src={Logo} alt='Mobikey Logo' className={`bg-light ${SideStyles.image}`}/>
-                <ul className={SideStyles.sidebarList}>
-                    <li>
-                        <NavLink to="/dashboard/statistics" className={SideStyles.navLink}>
-                            <div className={SideStyles.sideOption}>
-                                <i className={`fa fa-home ${SideStyles.sideIcons}`}></i>
-                                <h3 className={SideStyles.h3}>Dashboard</h3>
-                            </div>
-                        </NavLink>
-                    </li>
-                    <li className={`dropdown ${SideStyles.dropdown}`}>
-                        <a role='button' onClick={toggleLeaveDropdown} className={SideStyles.navLink}>
-                            <div className={SideStyles.sideOption}>
-                                <i className={`fa fa-calendar ${SideStyles.sideIcons}`}></i>
-                                <h3 className={SideStyles.h3}>Leave</h3>
-                                {leaveDropdown 
-                                    ? 
-                                        (<i className={`fa fa-angle-down ${SideStyles.sideIcons} ${SideStyles.dropdownIcon}`}></i>)
-                                    : 
-                                        (<i className={`fa fa-angle-right ${SideStyles.sideIcons} ${SideStyles.dropdownIcon}`}></i>) 
-                                }
-                            </div>
-                        </a>
-                        {leaveDropdown && (
-                            <ul className={`dropdown-content ${SideStyles.dropdownContent} ${SideStyles.sidebarList}`}>
-                                <li>
-                                    <NavLink to="/dashboard/leave-form" className={SideStyles.navLink}>
-                                        <div className={SideStyles.sideOption}>
-                                            <i className={`fa fa-calendar-plus-o ${SideStyles.sideIcons}`}></i>
-                                            <h3 className={SideStyles.h3}>Apply Leave</h3>
-                                        </div>
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/leave-history" className={SideStyles.navLink}>
-                                        <div className={SideStyles.sideOption}>
-                                            <i className={`fa fa-table ${SideStyles.sideIcons}`}></i>
-                                            <h3 className={SideStyles.h3}>Leave History</h3>
-                                        </div>
-                                    </NavLink>
-                                </li>
-                            </ul>
-                        )}
-                    </li>
-                    <li>
-                        <NavLink to="/dashboard/pending" className={SideStyles.navLink}>
-                            <div className={SideStyles.sideOption}>
-                                <i className={`fa fa-clock-o ${SideStyles.sideIcons}`}></i>
-                                <h3 className={SideStyles.h3}>Employee Requests</h3>
-                            </div>
-                        </NavLink>
-                    </li>
-                    <li className="dropdown">
-                        <a role='button' onClick={toggleEmployeeeDropdown} className={SideStyles.navLink}>
-                            <div className={SideStyles.sideOption}>
-                                <i className={`fa fa-user ${SideStyles.sideIcons}`}></i>
-                                <h3 className={SideStyles.h3}>Employees</h3>
-                                {employeeDropdown 
-                                    ? 
-                                        (<i className={`fa fa-angle-down ${SideStyles.sideIcons} ${SideStyles.dropdownIcon}`}></i>) 
-                                    : 
-                                        (<i className={`fa fa-angle-right ${SideStyles.sideIcons} ${SideStyles.dropdownIcon}`}></i>) 
-                                }
-                            </div>
-                        </a>
-                        {employeeDropdown && (
-                            <ul className={`dropdown-content ${SideStyles.dropdownContent} ${SideStyles.sidebarList}`}>
-                                <li>
-                                    <NavLink to="/dashboard/new-employee" className={SideStyles.navLink}>
-                                        <div className={SideStyles.sideOption}>
-                                            <i className={`fa fa-user-plus ${SideStyles.sideIcons}`}></i>
-                                            <h3 className={SideStyles.h3}>New Employee</h3>
-                                        </div>
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/view-employees" className={SideStyles.navLink}>
-                                        <div className={SideStyles.sideOption}>
-                                            <i className={`fa fa-users ${SideStyles.sideIcons}`}></i>
-                                            <h3 className={SideStyles.h3}>All Employees</h3>
-                                        </div>
-                                    </NavLink>
-                                </li>
-                            </ul>
-                        )}
-                    </li>
-                </ul>
-            </aside>
+            <Sidebar className={`sidebar_custom ${sidebarOpen ? "sidebar-open": ''}`}>
+                <SidebarHeader>
+                    <SidebarLogo img={Logo}/>
+                </SidebarHeader>
+                <SidebarBody>
+                    <MenuBar>
+                        <MenuItem to='/dashboard/statistics' text='Dashboard' leftIcon={<FaHome/>}/>
+                        <SubMenuBar label="Leave" labelLeftIcon={<FaCalendarAlt/>}>
+                            <MenuItem to='/dashboard/leave-form'text='Apply Leave' leftIcon={<FaCalendarPlus/>}/>
+                            <MenuItem to='/dashboard/leave-history'text='Leave History' leftIcon={<FaRegCalendarAlt/>}/>
+                        </SubMenuBar>
+                        <SubMenuBar label="Employee Requests" labelLeftIcon={<MdOutlinePendingActions/>}>
+                            <MenuItem to='/dashboard/pending'text='Pending Requests' leftIcon={<MdOutlinePendingActions/>}/>
+                            <MenuItem to='/dashboard/all'text='All Requests' leftIcon={<FaTable />}/>
+                        </SubMenuBar>
+                        <SubMenuBar label="Employees" labelLeftIcon={<FaUser/>}>
+                            <MenuItem to='/dashboard/new-employee'text='New Employee' leftIcon={<FaUserPlus/>}/>
+                            <MenuItem to='/dashboard/view-employees'text='All Employees' leftIcon={<FaUsers/>}/>
+                        </SubMenuBar>
+                    </MenuBar>
+                </SidebarBody>
+            </Sidebar>
         </>
-    )
+     );
 }
  
-export default Sidebar;
+export default Side;
