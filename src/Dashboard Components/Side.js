43230e6eb5a51ae3f1e86c9b4c13a997c8eed857
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-31 17:53:54 +0300

    First change to see if the side bar works on Netlify

--- src/Dashboard Components/Side.js
+++ src/Dashboard Components/Side.js
@@ -6,6 +6,16 @@ import '../assets/css/sidebar.css'
 
 const Side = ({sidebarOpen}) => 
 {
+    const links={
+        dashboard: '/dashboard/statistics',
+        leaveForm: '/dashboard/leave-form',
+        leaveHistory: '/dashboard/leave-history',
+        pendingRequests: '/dashboard/pending',
+        allRequests: '/dashboard/all',
+        newEmployee: '/dashboard/new-employee',
+        viewEmployees: '/dashboard/view-employees'
+    }
+    console.log(links)
     return ( 
         <>
             <Sidebar className={`sidebar_custom ${sidebarOpen ? "sidebar-open": ''}`}>
@@ -16,16 +26,16 @@ const Side = ({sidebarOpen}) =>
                     <MenuBar>
                         <MenuItem to='/dashboard/statistics' text='Dashboard' leftIcon={<FaHome/>}/>
                         <SubMenuBar label="Leave" labelLeftIcon={<FaCalendarAlt/>}>
-                            <MenuItem to='/dashboard/leave-form'text='Apply Leave' leftIcon={<FaCalendarPlus/>}/>
-                            <MenuItem to='/dashboard/leave-history'text='Leave History' leftIcon={<FaRegCalendarAlt/>}/>
+                            <MenuItem to='/dashboard/leave-form' text='Apply Leave' leftIcon={<FaCalendarPlus/>}/>
+                            <MenuItem to='/dashboard/leave-history' text='Leave History' leftIcon={<FaRegCalendarAlt/>}/>
                         </SubMenuBar>
                         <SubMenuBar label="Employee Requests" labelLeftIcon={<MdOutlinePendingActions/>}>
-                            <MenuItem to='/dashboard/pending'text='Pending Requests' leftIcon={<MdOutlinePendingActions/>}/>
-                            <MenuItem to='/dashboard/all'text='All Requests' leftIcon={<FaTable />}/>
+                            <MenuItem to='/dashboard/pending' text='Pending Requests' leftIcon={<MdOutlinePendingActions/>}/>
+                            <MenuItem to='/dashboard/all' text='All Requests' leftIcon={<FaTable />}/>
                         </SubMenuBar>
                         <SubMenuBar label="Employees" labelLeftIcon={<FaUser/>}>
-                            <MenuItem to='/dashboard/new-employee'text='New Employee' leftIcon={<FaUserPlus/>}/>
-                            <MenuItem to='/dashboard/view-employees'text='All Employees' leftIcon={<FaUsers/>}/>
+                            <MenuItem to='/dashboard/new-employee' text='New Employee' leftIcon={<FaUserPlus/>}/>
+                            <MenuItem to='/dashboard/view-employees' text='All Employees' leftIcon={<FaUsers/>}/>
                         </SubMenuBar>
                     </MenuBar>
                 </SidebarBody>
