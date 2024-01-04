0d2c9cf5c9a87c7fe5a5c7a8e82f73ea36cfe8af
Samuel Muigai <ndungusam01@gmail.com>
2024-01-04 11:51:52 +0300

    Changed the start and end dates of the date pickers and added a select group for half/full day leave

--- src/Dashboard Pages/Leave Form.js
+++ src/Dashboard Pages/Leave Form.js
@@ -3,16 +3,25 @@ import { useState } from "react"
 const LeaveForm = () => 
 {
     const d=new Date()
-    const currentYear=d.getFullYear()
-    const month=d.getMonth() + 1
-    const date=d.getDate()
-
-    let minDateRange=`${currentYear}-${month}-${date}`
+    let currentYear=d.getFullYear()
+    let month=d.getMonth() + 1
+    let date=d.getDate()
+    let minDateRange=""
     const maxDateRange=currentYear + "-12-31"
+
+    if(month <= 9)
+    {
+        minDateRange=`${currentYear}-0${month}-0${date}`
+    }
+    else
+    {
+        minDateRange=`${currentYear}-${month}-${date}`
+    }
 
     const [leaveFormData, setLeaveFormData]=useState(
         {
             leaveType: "",
+            days: "",
             startDate: "",
             endDate: "",
             numDays: "",
@@ -30,11 +39,6 @@ const LeaveForm = () =>
                 ...leaveFormData,
                 [key]: value
             })
-
-        if(key === "leaveStartDate")
-        {
-            minDateRange=leaveFormData.leaveStartDate
-        }
     }
 
     let fileRequired=""
@@ -50,13 +54,13 @@ const LeaveForm = () =>
     
     return ( 
         <>
-            <form action="" className="row g-4 mx-2">
+            <form action="" className="row g-4 mx-3">
                 <h1 className="text-center text-uppercase">Leave Request Form</h1>
                 <div className="col-md-12">
                     <label htmlFor="availableDays" className="form-label fs-5">Available Leave Days</label>
                     <input type="text" id="availableDays" className="form-control" placeholder="Available Leave Days" readOnly/>
                 </div>
-                <div className="col-md-12">
+                <div className="col-md-6">
                     <label htmlFor="leaveType" className="form-label fs-5">Leave Type</label>
                     <select id="leaveType" className="form-select" value={leaveFormData.leaveType} onChange={handleInputChange} required>
                         <option value="">Select Leave Type</option>
@@ -66,13 +70,20 @@ const LeaveForm = () =>
                         <option value="maternity">Maternity Leave</option>
                     </select>
                 </div>
+                <div className="col-md-6">
+                    <label htmlFor="days" className="form-label fs-5">Half Day/Full Day</label>
+                    <select id="days" className="form-select" value={leaveFormData.days} onChange={handleInputChange} required>
+                        <option value="half">Half Day</option>
+                        <option value="full">Full Day</option>
+                    </select>
+                </div>
                 <div className="col-md-4">
                     <label htmlFor="startDate" className="form-label fs-5">Start Date</label>
                     <input type="date" id="startDate" min={minDateRange} max={maxDateRange} className="form-control" value={leaveFormData.startDate} onChange={handleInputChange} required/>
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="endDate" className="form-label fs-5">End Date</label>
-                    <input type="date" id="endDate" min={minDateRange} max={maxDateRange} className="form-control" value={leaveFormData.endDate} onChange={handleInputChange} required/>
+                    <input type="date" id="endDate" min={leaveFormData.startDate} max={maxDateRange} className="form-control" value={leaveFormData.endDate} onChange={handleInputChange} required/>
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="numDays" className="form-label fs-5">Number of days</label>
