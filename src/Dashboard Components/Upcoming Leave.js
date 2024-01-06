cfd589338e4334196cf94d12fcdc5ec468ca9f3c
Samuel Muigai <ndungusam01@gmail.com>
2024-01-06 18:00:34 +0300

    Add upcoming leave table and styled it.

--- src/Dashboard Components/Upcoming Leave.js
+++ src/Dashboard Components/Upcoming Leave.js
@@ -5,6 +5,32 @@ const UpcomingLeave = () =>
     return ( 
         <div className="upcoming">
             <h1>Upcoming department leave</h1>
+            <table className='table table-striped table-bordered'>
+                <thead>
+                    <tr>
+                        <th scope="col">Employee Name</th>
+                        <th scope="col">Start Date</th>
+                        <th scope="col">End Date</th>
+                        <th scope="col">Number of days</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td data-label="Employee Name">John Doe</td>
+                        <td data-label="Start Date">2023-12-09</td>
+                        <td data-label="End Date">2023-12-15</td>
+                        <td data-label="Number of days">6</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td data-label="Employee Name">John Doe</td>
+                        <td data-label="Start Date">2023-12-09</td>
+                        <td data-label="End Date">2023-12-15</td>
+                        <td data-label="Number of days">6</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
      );
 }
