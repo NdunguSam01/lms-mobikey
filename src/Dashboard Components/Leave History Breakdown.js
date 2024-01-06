94601974d4e8ae518294a62e1f0862c768758c85
Samuel Muigai <ndungusam01@gmail.com>
2024-01-06 21:08:47 +0300

    Added a pie chart to the statistics page

--- src/Dashboard Components/Leave History Breakdown.js
+++ src/Dashboard Components/Leave History Breakdown.js
@@ -1,10 +1,23 @@
+import { PieChart } from '@mui/x-charts/PieChart';
 import  '../assets/css/statistics.css'
+
 const LeaveBreakdown = () => 
 {
+    const values=
+    {
+        approved: 10,
+        pending: 10,
+        rejected: 10
+    }
+    const chartData=[
+        { id: 0,value: values.approved,label: "Approved",color: "#4CAF50" },
+        {id: 1,value: values.pending,label:"Pending", color: "#FFC107" },
+        {id: 2,value: values.rejected, label:"Rejected",color: "#FF5722"}
+    ]
     return ( 
         <div className="history">
             <h1>Leave History Breakdown</h1>
-            
+            <PieChart series={[{data: chartData,outerRadius: 100}]} width={400} height={200}/>
         </div>
      );
 }
