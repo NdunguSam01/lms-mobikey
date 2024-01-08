e6ba4139f4304e77717ca36971f566a0a5d46ff1
Samuel Muigai <ndungusam01@gmail.com>
2024-01-08 18:02:40 +0300

    Added data validation such that all required input fields need to contain data so that the leave days can be calculated

--- src/Calculations/Leave Days.js
+++ src/Calculations/Leave Days.js
@@ -6,55 +6,58 @@ const LeaveDays = (startDate, endDate, leaveType, leaveDuration) =>
     leaveDuration=leaveDuration.toLowerCase()
 
     let dateDifference=(endDate-startDate) / (24 * 60 * 60 * 1000) + 1 
-    
-    if(leaveType === "maternity" || leaveType=== "paternity")
-    {
-            return dateDifference
-    }
-    else if (leaveType === "normal" || leaveType=== "sick")
-    {
-        let currentDate=startDate
-        let datesArray=[]
-        while (currentDate <= endDate)
-        {
-            datesArray.push(new Date(currentDate))
-            currentDate.setDate(currentDate.getDate() + 1)
-        }
-            
-        let saturdayCount=0
-        let sundayCount=0
 
-        datesArray.forEach(date =>
-            {
-                if(date.getDay()===0)
-                {
-                        sundayCount+=1
-                }
-                else if(date.getDay()===6)
-                {
-                    saturdayCount+=1
-                }
-            })
-            
-        if(leaveDuration === '')
-        {
-            dateDifference=0
-        }
-        else if (leaveDuration === "full")
-        {
-            dateDifference=dateDifference - (saturdayCount *0.5) - (sundayCount *1)
-        }
-        else if(leaveDuration === "half")
-        {
-            dateDifference=(dateDifference - (sundayCount *1))/2 
-        }
-            
-        return dateDifference
+    if(leaveType === "" || leaveDuration === "" || startDate === "" || endDate === "")
+    {
+        dateDifference=0
     }
     else
     {
-        return 0
+       if(leaveType === "maternity" || leaveType=== "paternity")
+        {
+            return dateDifference
+        }
+        else
+        {
+            let currentDate=startDate
+            let datesArray=[]
+            while (currentDate <= endDate)
+            {
+                datesArray.push(new Date(currentDate))
+                currentDate.setDate(currentDate.getDate() + 1)
+            }
+            
+            let saturdayCount=0
+            let sundayCount=0
+
+            datesArray.forEach(date =>
+                {
+                    if(date.getDay()===0)
+                    {
+                        sundayCount+=1
+                    }
+                    else if(date.getDay()===6)
+                    {
+                        saturdayCount+=1
+                    }
+                })
+            
+            if(leaveDuration === '')
+            {
+                dateDifference=0
+            }
+            else if (leaveDuration === "full")
+            {
+                dateDifference=dateDifference - (saturdayCount *0.5) - (sundayCount *1)
+            }
+            else if(leaveDuration === "half")
+            {
+                dateDifference=(dateDifference - (sundayCount *1))/2 
+            }
+        }
     }
+    
+    return dateDifference
 }
  
 export default LeaveDays;
