86f40894aaa9d27497ea84255ac89f43b5384463
Samuel Muigai <ndungusam01@gmail.com>
2024-01-08 17:45:22 +0300

    Modified the code such that the leave days are calculated after a user has- filled in the leave type, leave duration, start and end dates

--- src/Calculations/Leave Days.js
+++ src/Calculations/Leave Days.js
@@ -8,45 +8,53 @@ const LeaveDays = (startDate, endDate, leaveType, leaveDuration) =>
     let dateDifference=(endDate-startDate) / (24 * 60 * 60 * 1000) + 1 
     
     if(leaveType === "maternity" || leaveType=== "paternity")
-        {
+    {
             return dateDifference
-        }
-        else
+    }
+    else if (leaveType === "normal" || leaveType=== "sick")
+    {
+        let currentDate=startDate
+        let datesArray=[]
+        while (currentDate <= endDate)
         {
-            let currentDate=startDate
-            let datesArray=[]
-            while (currentDate <= endDate)
-            {
-                datesArray.push(new Date(currentDate))
-                currentDate.setDate(currentDate.getDate() + 1)
-            }
+            datesArray.push(new Date(currentDate))
+            currentDate.setDate(currentDate.getDate() + 1)
+        }
             
-            let saturdayCount=0
-            let sundayCount=0
+        let saturdayCount=0
+        let sundayCount=0
 
-            datesArray.forEach(date =>
+        datesArray.forEach(date =>
+            {
+                if(date.getDay()===0)
                 {
-                    if(date.getDay()===0)
-                    {
                         sundayCount+=1
-                    }
-                    else if(date.getDay()===6)
-                    {
-                        saturdayCount+=1
-                    }
-                })
-
-            if (leaveDuration === "full")
-            {
-                dateDifference=dateDifference - (saturdayCount *0.5) - (sundayCount *1)
-            }
-            else if(leaveDuration === "half")
-            {
-                dateDifference=(dateDifference - (sundayCount *1))/2 
-            }
+                }
+                else if(date.getDay()===6)
+                {
+                    saturdayCount+=1
+                }
+            })
             
-            return dateDifference
+        if(leaveDuration === '')
+        {
+            dateDifference=0
         }
+        else if (leaveDuration === "full")
+        {
+            dateDifference=dateDifference - (saturdayCount *0.5) - (sundayCount *1)
+        }
+        else if(leaveDuration === "half")
+        {
+            dateDifference=(dateDifference - (sundayCount *1))/2 
+        }
+            
+        return dateDifference
+    }
+    else
+    {
+        return 0
+    }
 }
  
 export default LeaveDays;
