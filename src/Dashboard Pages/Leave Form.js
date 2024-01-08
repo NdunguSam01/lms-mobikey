1a8dd16ee4c87b2b74e89e32ba3e29caa4fb4385
Samuel Muigai <ndungusam01@gmail.com>
2024-01-08 11:41:20 +0300

    Added input fields that show the various leave types and their remaining days

--- src/Dashboard Pages/Leave Form.js
+++ src/Dashboard Pages/Leave Form.js
@@ -61,26 +61,26 @@ const LeaveForm = () =>
     
     return ( 
         <>
-            <form action="" className="row g-4">
+            <form action="" className="row g-3">
                 <h2 className="text-uppercase text-decoration-underline">Available Leave days</h2>
-                <div className="col-md-3">
+                <div className="col-md-3 mt-2">
                     <label htmlFor="normalLeaveDays" className="form-label fs-5">Normal Leave</label>
                     <input type="number" id="normalLeaveDays" className="form-control" value={21} readOnly/>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-3 mt-2">
                     <label htmlFor="sickLeaveDays" className="form-label fs-5">Sick Leave</label>
                     <input type="number" id="sickLeaveDays" className="form-control" value={10} readOnly/>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-3 mt-2">
                     <label htmlFor="paternityLeaveDays" className="form-label fs-5">Paternity Leave</label>
                     <input type="number" id="paternityLeaveDays" className="form-control" value={14} readOnly/>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-3 mt-2">
                     <label htmlFor="maternityLeaveDys" className="form-label fs-5">Maternity Leave</label>
                     <input type="number" id="maternityLeaveDys" className="form-control" value={90} readOnly/>
                 </div>
                 <h2 className="text-uppercase text-decoration-underline mt-4">Leave request details</h2>
-                <div className="col-md-6 mt-2">
+                <div className="col-md-6 mt-3">
                     <label htmlFor="leaveType" className="form-label fs-5">Leave Type</label>
                     <select id="leaveType" className="form-select" value={leaveFormData.leaveType} onChange={handleInputChange} required>
                         <option value="normal">Normal Leave</option>
@@ -89,30 +89,30 @@ const LeaveForm = () =>
                         <option value="maternity">Maternity Leave</option>
                     </select>
                 </div>
-                <div className="col-md-6 mt-2">
+                <div className="col-md-6 mt-3">
                     <label htmlFor="leaveDuration" className="form-label fs-5">Leave Duration</label>
                     <select id="leaveDuration" className="form-select" value={leaveFormData.leaveDuration} onChange={handleInputChange} required>
                         <option value="full">Full Day</option>
                         <option value="half">Half Day</option>
                     </select>
                 </div>
-                <div className="col-md-4 mt-2">
+                <div className="col-md-4 mt-3">
                     <label htmlFor="startDate" className="form-label fs-5">Start Date</label>
                     <input type="date" id="startDate" min={minDateRange} max={maxDateRange} className="form-control" value={leaveFormData.startDate} onChange={handleInputChange} required/>
                 </div>
-                <div className="col-md-4 mt-2">
+                <div className="col-md-4 mt-3">
                     <label htmlFor="endDate" className="form-label fs-5">End Date</label>
                     <input type="date" id="endDate" min={leaveFormData.startDate} max={maxDateRange} className="form-control" value={leaveFormData.endDate} onChange={handleInputChange} required/>
                 </div>
-                <div className="col-md-4 mt-2">
+                <div className="col-md-4 mt-3">
                     <label htmlFor="numDays" className="form-label fs-5">Number of days</label>
                     <input type="number" id="numDays" className="form-control" required value={leaveFormData.numDays} onChange={handleInputChange} readOnly/>
                 </div>
-                <div className="col-md-6 mt-2">
+                <div className="col-md-6 mt-3">
                     <label htmlFor="fileAttachment" className="form-label fs-5">Attach file</label>
                     <input type="file" id="fileAttachment" className="form-control" accept="image/*, application/pdf" value={leaveFormData.fileAttachment} onChange={handleInputChange} required={fileRequired}/>
                 </div>
-                <div className="col-md-6 mt-2">
+                <div className="col-md-6 mt-32">
                     <label htmlFor="reason" className="form-label fs-5">Reason for leave</label>
                     <textarea name="" id="reason" cols="20" rows="1" className="form-control" value={leaveFormData.reason} onChange={handleInputChange} placeholder="Reason for leave (Optional)"></textarea>
                 </div>
